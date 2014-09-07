de6e5135e11aee9771cc9defb13da5f0007fb9f4
andrewray <evilkidder@gmail.com>
2014-09-07 21:57:11 +0100

    fix a couple of comment typos and trigger travis

--- src/cookie.mli
+++ src/cookie.mli
@@ -1,9 +1,13 @@
+(** Simple get/set cookie routines for js_of_ocaml.
+ 
+    Based on javascript code at http://www.quirksmode.org/js/cookies.html *)
+
 (** [set name value] creates (or replaces) a cookie.
 
     [days] sets the expiry date days in the future.  By default it is not set 
     and should be destroyed when the browser is closed.
 
-    [path] sets the domain of the cookie and defaults the '/'. *)
+    [path] sets the domain of the cookie and defaults to '/'. *)
 val set : ?days:float -> ?path:string -> string -> string -> unit
 
 (** get value of cookie with given name *)
